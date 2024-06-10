5a26eaf0096535098686bf66b153d70207c6316d
Rodrigo <rodrigobaesa@gmail.com>
2024-06-09 23:46:38 -0300

    commit inicial

---
+++ passgen/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.forms.widgets import NumberInput
+
+
+class RangeInput(NumberInput):
+    input_type = 'range'
+
+
+class PassGenForm(forms.Form):
+    length = forms.IntegerField(
+        label='Password length',
+        required=True,
+        widget=RangeInput(attrs={'min': '1', 'max': '64', 'class': 'range', 'id': 'lengthRangeInput'})
+    )
+    include_symbols = forms.BooleanField(
+        label='Include symbols (@&$!#?)',
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'checkboxInput'})
+    )
+    include_similar_characters = forms.BooleanField(
+        label='Include similar characters (e.g. 0O l1 2Z)',
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'checkboxInput'})
+    )
---
+++ passgen/views.py
@@ -0,0 +1,31 @@
+import string
+import re
+import random
+
+from django.shortcuts import render
+from django.views import View
+from .forms import PassGenForm
+
+
+class Index(View):
+    def get(self, request):
+        form = PassGenForm()
+
+        context = {'form': form}
+        return render(request, 'passgen/index.html', context)
+
+    def post(self, request):
+        form = PassGenForm(request.POST)
+
+        if form.is_valid():
+            available_characters = string.ascii_letters + string.digits
+            if form.cleaned_data['include_symbols']:
+                available_characters += string.punctuation
+
+            if form.cleaned_data['include_similar_characters']:
+                ambiguous_characters = ['Z', '2', 'l', '1', '0', 'O', 'o']
+                available_characters = re.sub('|'.join(ambiguous_characters), ' ', available_characters)
+
+            password = ''.join(random.choice(available_characters) for i in range(form.cleaned_data['length']))
+            print(password)
+        return render(request, 'passgen/index.html')
